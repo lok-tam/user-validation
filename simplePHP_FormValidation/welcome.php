7f31cfb0a87a46e399c12d72592b8dcd4963c18d
LokB <lb.tamang77@gmail.com>
2017-10-08 15:18:15 -0500

    first commit - adding all files

---
+++ simplePHP_FormValidation/welcome.php
@@ -0,0 +1,24 @@
+<!-- 
+<html>
+<head>
+	<title>Welcome Page</title>
+</head>
+<body>
+		Welcome <?php echo $_GET["name"]; ?> <br>
+		Your email address is <?php echo $_GET["email"]; ?><br>
+
+</body>
+</html> -->
+
+<?php 
+
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+
+</body>
+</html>
+
+?>
